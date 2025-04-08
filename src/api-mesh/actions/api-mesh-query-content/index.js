9b4b1cb4320615b7205c7feab3e7b2e322484119
MartineBoulanger <martine.boulanger@vaimo.com>
2025-04-08 09:28:43 +0200

    added grapql request package

--- src/api-mesh/actions/api-mesh-query-content/index.js
+++ src/api-mesh/actions/api-mesh-query-content/index.js
@@ -1,6 +1,6 @@
 /*
-* <license header>
-*/
+ * <license header>
+ */
 
 /**
  * This is a sample action showcasing how to query API MESH
@@ -13,62 +13,71 @@
  *   - Make sure to validate these changes against your security requirements before deploying the action
  */
 
-
-const { GraphQLClient, gql } = require('graphql-request')
-const { Core } = require('@adobe/aio-sdk')
-const { errorResponse, getBearerToken, stringParameters, checkMissingRequestInputs } = require('../utils')
+const { GraphQLClient, gql } = require('graphql-request');
+const { Core } = require('@adobe/aio-sdk');
+const {
+  errorResponse,
+  getBearerToken,
+  stringParameters,
+  checkMissingRequestInputs,
+} = require('../utils');
 
 // main function that will be executed by Adobe I/O Runtime
-async function main (params) {
+async function main(params) {
   // create a Logger
-  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' })
+  const logger = Core.Logger('main', { level: params.LOG_LEVEL || 'info' });
 
   try {
     // 'info' is the default level if not set
-    logger.info('Calling the main action')
+    logger.info('Calling the main action');
 
     // log parameters, only if params.LOG_LEVEL === 'debug'
-    logger.debug(stringParameters(params))
+    logger.debug(stringParameters(params));
 
     // check for missing request input parameters and headers
-    const requiredParams = ['MESH_ID']
-    const requiredHeaders = ['Authorization']
-    const errorMessage = checkMissingRequestInputs(params, requiredParams, requiredHeaders)
+    const requiredParams = ['MESH_ID'];
+    const requiredHeaders = ['Authorization'];
+    const errorMessage = checkMissingRequestInputs(
+      params,
+      requiredParams,
+      requiredHeaders
+    );
     if (errorMessage) {
       // return and log client errors
-      return errorResponse(400, errorMessage, logger)
+      return errorResponse(400, errorMessage, logger);
     }
 
     // extract the user Bearer token from the Authorization header
-    const token = getBearerToken(params)
+    const token = getBearerToken(params);
 
-    const apiMeshEndpoint = `https://edge-graph.adobe.io/api/${params.MESH_ID}/graphql`
+    const apiMeshEndpoint = `https://www.graph.adobe.io/api/${params.MESH_ID}/graphql`;
     const query = gql`
       {
         storeConfig {
           store_name
         }
       }
-    `
+    `;
     // query content from API MESH
-    const client = new GraphQLClient(apiMeshEndpoint)
-    const data = await client.request(query)
+    const client = new GraphQLClient(apiMeshEndpoint);
+    const data = await client.request(query);
     // perform some business logic
-    data.storeConfig.store_name = '[App Builder]: ' + data.storeConfig.store_name
+    data.storeConfig.store_name =
+      '[App Builder]: ' + data.storeConfig.store_name;
     const response = {
       statusCode: 200,
-      body: data
-    }
+      body: data,
+    };
 
     // log the response status code
-    logger.info(`${response.statusCode}: successful request`)
-    return response
+    logger.info(`${response.statusCode}: successful request`);
+    return response;
   } catch (error) {
     // log any server errors
-    logger.error(error)
+    logger.error(error);
     // return with 500
-    return errorResponse(500, 'server error', logger)
+    return errorResponse(500, 'server error', logger);
   }
 }
 
-exports.main = main
+exports.main = main;
